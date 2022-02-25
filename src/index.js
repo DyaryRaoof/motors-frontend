7a6f975c273dd500d4e0d0cc54ce9b3962e50b83
DyaryRaoof <dyary2020@gmail.com>
2022-02-25 20:59:53 +0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import configureStore from './redux/configureStore';
+import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={configureStore}>
-      <App />
+    <Provider store={store} key="1">
+      <App key="2" />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
