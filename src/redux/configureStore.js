a3ef938375108918f8741dde2c097ee69fd7ff44
DyaryRaoof <dyary2020@gmail.com>
2022-03-01 17:40:31 +0300

    Fix motors fetch error from detail page and add user dispatch action

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,6 +3,11 @@ import reduxLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import motorsReducer from './motors/motors';
 import reservationsReducer from './reservations/reservations';
+import usersReducer from './users/users';
 
-const reducers = combineReducers({ motors: motorsReducer, reservations: reservationsReducer });
+const reducers = combineReducers({
+  motors: motorsReducer,
+  reservations: reservationsReducer,
+  usersReducer,
+});
 export default createStore(reducers, applyMiddleware(thunk, reduxLogger));
