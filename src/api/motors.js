7356f41639bce6a779c6ae635871aa7feca56a67
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-03-03 16:40:48 +0300

    Fix linters

--- src/api/motors.js
+++ src/api/motors.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { fetchMotors } from '../redux/motors/motors';
 import { fetchDetailMotors } from '../redux/motors/DetailMotors';
+import { addItem } from '../redux/motors/AddItem';
 
 const BASE_URL = 'http://localhost:3001/api/v1/motors';
 
@@ -13,4 +14,24 @@ export const getMotorsDetail = (id) => async (dispatch) => {
   fetch(`http://localhost:3001/api/v1/motors/${id}`)
     .then((res) => res.json())
     .then((resResponse) => dispatch(fetchDetailMotors(resResponse)));
+};
+
+export const AddItemHandler = (data) => async (dispatch) => {
+  const formData = new FormData();
+  formData.append('name', data.name);
+  formData.append('description', data.description);
+  formData.append('price', data.price);
+  formData.append('image', data.image);
+  await fetch('http://localhost:3001/api/v1/motors/create', {
+    method: 'POST',
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((resResponse) => dispatch(addItem(resResponse)));
+};
+
+export const deleteMotor = (id) => async () => {
+  fetch(`http://localhost:3001/api/v1/motor/${id}/destroy`)
+    .then((res) => res.json())
+    .then((resResponse) => console.log(resResponse));
 };
